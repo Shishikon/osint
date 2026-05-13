36cfbab929f95604f907cc90be3a9b3e0daac57f
Sherzod <sherzodrazzaqov16@gmail.com>
2026-05-13 23:23:45 +0500

    added database url

--- project/settings.py
+++ project/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://osint-osint.up.railway.app/',
+    'https://osint-osint.up.railway.app',
 ]
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -31,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'osint-osint.up.railway.app').split(',')
 
 # Application definition
 
